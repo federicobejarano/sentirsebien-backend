49feec6b7239a0d1a130b708b227f976e5d1eea5
Federico Bejarano <federicojuanbejarano@gmail.com>
2024-10-23 15:54:23 -0300

    añadir objetos de valor para servicio de logueo (capa de dominio)

---
+++ sentirsebien-backend/Domain/ValueObjects/AutorizacionUsuario.cs
@@ -0,0 +1,20 @@
+﻿using sentirsebien_backend.Domain.Entities;
+
+namespace sentirsebien_backend.Domain.ValueObjects
+{
+    public class AutorizacionUsuario
+    {
+        public int UserId { get; }
+        public string Email { get; }
+        public IReadOnlyCollection<Rol> Roles { get; }
+        public IReadOnlyCollection<Permiso> Permisos { get; }
+
+        public AutorizacionUsuario(int userId, string email, IReadOnlyCollection<Rol> roles, IReadOnlyCollection<Permiso> permisos)
+        {
+            UserId = userId;
+            Email = email ?? throw new ArgumentNullException(nameof(email));
+            Roles = roles ?? throw new ArgumentNullException(nameof(roles));
+            Permisos = permisos ?? throw new ArgumentNullException(nameof(permisos));
+        }
+    }
+}
