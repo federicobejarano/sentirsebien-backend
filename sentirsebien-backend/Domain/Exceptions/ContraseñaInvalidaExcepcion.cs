5cba8227f13716391fe9a7972020b5484676c31c
Federico Bejarano <federicojuanbejarano@gmail.com>
2024-10-23 23:57:20 -0300

    añadir excepciones

---
+++ sentirsebien-backend/Domain/Exceptions/ContraseñaInvalidaExcepcion.cs
@@ -0,0 +1,14 @@
+﻿namespace sentirsebien_backend.Domain.Exceptions
+{
+    public class ContraseñaInvalidaException : Exception
+    {
+        public ContraseñaInvalidaException()
+            : base("La contraseña proporcionada es incorrecta.") { }
+
+        public ContraseñaInvalidaException(string mensaje)
+            : base(mensaje) { }
+
+        public ContraseñaInvalidaException(string mensaje, Exception innerException)
+            : base(mensaje, innerException) { }
+    }
+}
---
+++ sentirsebien-backend/Domain/Exceptions/UsuarioNoEncontradoException.cs
@@ -0,0 +1,14 @@
+﻿namespace sentirsebien_backend.Domain.Exceptions
+{
+    public class UsuarioNoEncontradoException : Exception
+    {
+        public UsuarioNoEncontradoException()
+            : base("El usuario no ha sido encontrado.") { }
+
+        public UsuarioNoEncontradoException(string mensaje)
+            : base(mensaje) { }
+
+        public UsuarioNoEncontradoException(string mensaje, Exception innerException)
+            : base(mensaje, innerException) { }
+    }
+}
