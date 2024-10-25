ffee4740f2367eebdd9e494b4fd5177938ee9f75
Federico Bejarano <federicojuanbejarano@gmail.com>
2024-10-25 06:19:39 -0300

    añadir login response DTO (capa de APIs)

--- sentirsebien-backend/API/Dtos/LoginResponseDTO.cs
+++ sentirsebien-backend/API/Dtos/LoginResponseDTO.cs
@@ -1,9 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using sentirsebien_backend.Domain.ValueObjects;
+using System.ComponentModel.DataAnnotations;
 
 namespace sentirsebien_backend.Application.DTOs
 {
     public class LoginResponseDTO
     {
-        // implementar response
+        public string Token { get; private set; }
+        public DateTime FechaCreacion { get; private set; }
+        public DateTime FechaExpiracion { get; private set; }
+        public int UserId { get; private set; }
+        public string Email { get; private set; }
+        public IEnumerable<string> Roles { get; private set; }
+        public IEnumerable<string> Permisos { get; private set; }
+
+        public LoginResponseDTO(TokenAutenticacion token)
+        {
+            Token = token.Token;
+            FechaCreacion = token.FechaCreacion;
+            FechaExpiracion = token.FechaExpiracion;
+            UserId = token.UserId;
+            Email = token.Email;
+            Roles = token.Roles;
+            Permisos = token.Permisos;
+        }
     }
+
 }
